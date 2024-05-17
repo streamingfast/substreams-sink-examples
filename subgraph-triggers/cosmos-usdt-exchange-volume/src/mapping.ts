e1076a357f12a5f5d1e427a437e9c0d17cfbbe60
Enol Álvarez <enol826@gmail.com>
2024-05-17 12:40:01 +0200

    WIP

--- subgraph-triggers/cosmos-usdt-exchange-volume/src/mapping.ts
+++ subgraph-triggers/cosmos-usdt-exchange-volume/src/mapping.ts
@@ -10,20 +10,26 @@ export function handleExchanges(bytes: Uint8Array): void {
     const usdtExchangeList: USDTExchangeList = Protobuf.decode<USDTExchangeList>(bytes, USDTExchangeList.decode);
     const exchanges = usdtExchangeList.exchanges;
 
+    log.info("Protobuf decoded, length: {}", [exchanges.length.toString()]);
+
     let entity = USDTExchangeVolume.load(ID);
     if (entity == null) {
+        log.info("Entity not found, creating one...", []);
         entity = new USDTExchangeVolume(ID);
         entity.amount = '0';
     }
 
     for (let i = 0; i < exchanges.length; i++) {
         const exchange = exchanges[i];
+        log.info("Exchange with amount: {}", [exchange.amount]);
         const exchangeAmount = BigInt.fromString(exchange.amount);
 
         const entityAmount = BigInt.fromString(entity.amount);
         entityAmount.plus(exchangeAmount);
+        log.info("Amount added", []);
 
         entity.amount = entityAmount.toString();
         entity.save();
+        log.info("Entity saved", []);
     }
 }
