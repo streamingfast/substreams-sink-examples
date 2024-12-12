90d7fd5d1a8d60f839e54b7cd9d256b95495b2bd
Matthieu Vachon <matt@streamingfast.io>
2024-12-11 23:20:08 -0500

    Ported over Rust example

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This repository contains several examples of production-ready  sink applications to consume Subtreams package with any of the SDKs available:
 
-- [Go Substreams SDK Example](go)
-- [JavaScript Substreams SDK Examples](javascript)
-    - [Node Example](javascript/node)
-    - [Web Example](javascript/web)
+- [Go Substreams SDK Example](go/README.md)
+- [JavaScript Substreams SDK Examples](javascript/README.md)
+    - [Node Example](javascript/node/README.md)
+    - [Web Example](javascript/web/README.md)
+- [Rust](rust/README.md)
---
+++ rust/src/main.rs
@@ -0,0 +1,205 @@
+use anyhow::{format_err, Context, Error};
+use chrono::DateTime;
+use futures03::StreamExt;
+use pb::sf::substreams::rpc::v2::{BlockScopedData, BlockUndoSignal};
+use pb::sf::substreams::v1::Package;
+
+use prost::Message;
+use std::{env, process::exit, sync::Arc};
+use substreams::SubstreamsEndpoint;
+use substreams_stream::{BlockResponse, SubstreamsStream};
+
+mod pb;
+mod substreams;
+mod substreams_stream;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let args = env::args();
+    if args.len() < 4 || args.len() > 5 {
+        println!("usage: stream <endpoint> <spkg> <module> [<start>:<stop>]");
+        println!();
+        println!("The environment variable SUBSTREAMS_API_TOKEN must be set also");
+        println!("and should contain a valid Substream API token.");
+        exit(1);
+    }
+
+    let endpoint_url = env::args().nth(1).unwrap();
+    let package_file = env::args().nth(2).unwrap();
+    let module_name = env::args().nth(3).unwrap();
+
+    let token_env = env::var("SUBSTREAMS_API_TOKEN").unwrap_or("".to_string());
+    let mut token: Option<String> = None;
+    if token_env.len() > 0 {
+        token = Some(token_env);
+    }
+
+    let package = read_package(&package_file).await?;
+    let block_range = read_block_range(&package, &module_name)?;
+    let endpoint = Arc::new(SubstreamsEndpoint::new(&endpoint_url, token).await?);
+
+    let cursor: Option<String> = load_persisted_cursor()?;
+
+    let mut stream = SubstreamsStream::new(
+        endpoint.clone(),
+        cursor,
+        package.modules.clone(),
+        module_name.to_string(),
+        block_range.0,
+        block_range.1,
+    );
+
+    loop {
+        match stream.next().await {
+            None => {
+                println!("Stream consumed");
+                break;
+            }
+            Some(Ok(BlockResponse::New(data))) => {
+                process_block_scoped_data(&data)?;
+                persist_cursor(data.cursor)?;
+            }
+            Some(Ok(BlockResponse::Undo(undo_signal))) => {
+                process_block_undo_signal(&undo_signal)?;
+                persist_cursor(undo_signal.last_valid_cursor)?;
+            }
+            Some(Err(err)) => {
+                println!();
+                println!("Stream terminated with error");
+                println!("{:?}", err);
+                exit(1);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn process_block_scoped_data(data: &BlockScopedData) -> Result<(), Error> {
+    let output = data.output.as_ref().unwrap().map_output.as_ref().unwrap();
+
+    // You can decode the actual Any type received using this code:
+    //
+    //     let value = GeneratedStructName::decode(output.value.as_slice())?;
+    //
+    // Where GeneratedStructName is the Rust code generated for the Protobuf representing
+    // your type, so you will need generate it using `substreams protogen` and import it from the
+    // `src/pb` folder.
+
+    let clock = data.clock.as_ref().unwrap();
+    let timestamp = clock.timestamp.as_ref().unwrap();
+    let date = DateTime::from_timestamp(timestamp.seconds, timestamp.nanos as u32)
+        .expect("received timestamp should always be valid");
+
+    println!(
+        "Block #{} - Payload {} ({} bytes) - Drift {}s",
+        clock.number,
+        output.type_url.replace("type.googleapis.com/", ""),
+        output.value.len(),
+        date.signed_duration_since(chrono::offset::Utc::now())
+            .num_seconds()
+            * -1
+    );
+
+    Ok(())
+}
+
+fn process_block_undo_signal(_undo_signal: &BlockUndoSignal) -> Result<(), anyhow::Error> {
+    // `BlockUndoSignal` must be treated as "delete every data that has been recorded after
+    // block height specified by block in BlockUndoSignal". In the example above, this means
+    // you must delete changes done by `Block #7b` and `Block #6b`. The exact details depends
+    // on your own logic. If for example all your added record contain a block number, a
+    // simple way is to do `delete all records where block_num > 5` which is the block num
+    // received in the `BlockUndoSignal` (this is true for append only records, so when only `INSERT` are allowed).
+    unimplemented!("you must implement some kind of block undo handling, or request only final blocks (tweak substreams_stream.rs)")
+}
+
+fn persist_cursor(_cursor: String) -> Result<(), anyhow::Error> {
+    // FIXME: Handling of the cursor is missing here. It should be saved each time
+    // a full block has been correctly processed/persisted. The saving location
+    // is your responsibility.
+    //
+    // By making it persistent, we ensure that if we crash, on startup we are
+    // going to read it back from database and start back our SubstreamsStream
+    // with it ensuring we are continuously streaming without ever losing a single
+    // element.
+    Ok(())
+}
+
+fn load_persisted_cursor() -> Result<Option<String>, anyhow::Error> {
+    // FIXME: Handling of the cursor is missing here. It should be loaded from
+    // somewhere (local file, database, cloud storage) and then `SubstreamStream` will
+    // be able correctly resume from the right block.
+    Ok(None)
+}
+
+fn read_block_range(pkg: &Package, module_name: &str) -> Result<(i64, u64), anyhow::Error> {
+    let module = pkg
+        .modules
+        .as_ref()
+        .unwrap()
+        .modules
+        .iter()
+        .find(|m| m.name == module_name)
+        .ok_or_else(|| format_err!("module '{}' not found in package", module_name))?;
+
+    let mut input: String = "".to_string();
+    if let Some(range) = env::args().nth(4) {
+        input = range;
+    };
+
+    let (prefix, suffix) = match input.split_once(":") {
+        Some((prefix, suffix)) => (prefix.to_string(), suffix.to_string()),
+        None => ("".to_string(), input),
+    };
+
+    let start: i64 = match prefix.as_str() {
+        "" => module.initial_block as i64,
+        x if x.starts_with("+") => {
+            let block_count = x
+                .trim_start_matches("+")
+                .parse::<u64>()
+                .context("argument <stop> is not a valid integer")?;
+
+            (module.initial_block + block_count) as i64
+        }
+        x => x
+            .parse::<i64>()
+            .context("argument <start> is not a valid integer")?,
+    };
+
+    let stop: u64 = match suffix.as_str() {
+        "" => 0,
+        "-" => 0,
+        x if x.starts_with("+") => {
+            let block_count = x
+                .trim_start_matches("+")
+                .parse::<u64>()
+                .context("argument <stop> is not a valid integer")?;
+
+            start as u64 + block_count
+        }
+        x => x
+            .parse::<u64>()
+            .context("argument <stop> is not a valid integer")?,
+    };
+
+    return Ok((start, stop));
+}
+
+async fn read_package(input: &str) -> Result<Package, anyhow::Error> {
+    if input.starts_with("http") {
+        return read_http_package(input).await;
+    }
+
+    // Assume it's a local file
+    let content =
+        std::fs::read(input).context(format_err!("read package from file '{}'", input))?;
+    Package::decode(content.as_ref()).context("decode command")
+}
+
+async fn read_http_package(input: &str) -> Result<Package, anyhow::Error> {
+    let body = reqwest::get(input).await?.bytes().await?;
+
+    Package::decode(body).context("decode command")
+}
